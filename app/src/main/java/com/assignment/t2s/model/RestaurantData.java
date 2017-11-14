38178119360b88e37ae23f6298b2c8740b593c97
Vivek CA <vivek@ooyala.com>
2017-11-14 12:57:05 +0530

    Initial commit

---
+++ app/src/main/java/com/assignment/t2s/model/RestaurantData.java
@@ -0,0 +1,49 @@
+package com.assignment.t2s.model;
+
+/**
+ * Created by vivek on 08/11/17.
+ */
+
+public class RestaurantData {
+    String restName, restAddress, restLocation, restPhoneNo, restTimings;
+
+    public String getRestName() {
+        return restName;
+    }
+
+    public void setRestName(String restName) {
+        this.restName = restName;
+    }
+
+    public String getRestAddress() {
+        return restAddress;
+    }
+
+    public void setRestAddress(String restAddress) {
+        this.restAddress = restAddress;
+    }
+
+    public String getRestLocation() {
+        return restLocation;
+    }
+
+    public void setRestLocation(String restLocation) {
+        this.restLocation = restLocation;
+    }
+
+    public String getRestPhoneNo() {
+        return restPhoneNo;
+    }
+
+    public void setRestPhoneNo(String restPhoneNo) {
+        this.restPhoneNo = restPhoneNo;
+    }
+
+    public String getRestTimings() {
+        return restTimings;
+    }
+
+    public void setRestTimings(String restTimings) {
+        this.restTimings = restTimings;
+    }
+}
---
+++ app/src/main/java/com/assignment/t2s/utils/NetworkHelper.java
@@ -0,0 +1,37 @@
+package com.assignment.t2s.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by vivek on 08/11/17.
+ */
+
+public class NetworkHelper {
+
+    public static String SERVER_URL = "https://test-api.t2scdn.com/";
+
+    public static String SERVER_GET_RESTAURANT = "?store=";
+
+
+    private static RequestQueue mRequestQueue;
+
+    public static RequestQueue getRequestQueue(Context context) {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(context);
+        }
+        return mRequestQueue;
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+}
